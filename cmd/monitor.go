bceeef9cba6ec8238e80d371491b6b64fa2abbc6
Joshua Blanch <42494981+JoshuaGabriel@users.noreply.github.com>
2024-12-22 09:02:08 +0800

    cmd: setup cli command (#2)
    * cmd: setup cli command
    
    Signed-off-by: Joshua Blanch <joshuamblanch@gmail.com>

---
+++ cmd/monitor.go
@@ -0,0 +1,80 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/rivo/tview"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/shirou/gopsutil/v3/process"
+	"github.com/urfave/cli/v2"
+	"sort"
+	"time"
+)
+
+func cmdMonitor() *cli.Command {
+	return &cli.Command{
+		Name:   "profile",
+		Action: profile,
+		Usage:  "Show reousrce usage of system",
+		Description: `
+Resource monitoring tools
+
+Examples:
+# Monitor real time operations
+$ goup profile 
+
+`,
+	}
+}
+func profile(ctx *cli.Context) error {
+
+	app := tview.NewApplication()
+	textView := tview.NewTextView().SetDynamicColors(true)
+
+	go func() {
+		for {
+			// Fetch metrics
+			cpuPercent, _ := cpu.Percent(0, false)
+			memStats, _ := mem.VirtualMemory()
+
+			// Fetch processes
+			procs, _ := process.Processes()
+			var procStats []string
+			for _, p := range procs {
+				name, _ := p.Name()
+				cpu, _ := p.CPUPercent()
+				procStats = append(procStats, fmt.Sprintf("%-20s %.2f%%", name, cpu))
+			}
+
+			// Sort processes by CPU usage
+			sort.Slice(procStats, func(i, j int) bool {
+				return procStats[i] > procStats[j]
+			})
+
+			// Display top 5 processes
+			topProcs := ""
+			for i := 0; i < 5 && i < len(procStats); i++ {
+				topProcs += procStats[i] + "\n"
+			}
+
+			// Update display
+			stats := fmt.Sprintf(
+				"[yellow]CPU Usage:[white] %.2f%%\n[yellow]Memory Usage:[white] %.2f%% of %.2fGB\n\n[yellow]Top Processes:\n[white]%s",
+				cpuPercent[0],
+				memStats.UsedPercent,
+				float64(memStats.Total)/1e9,
+				topProcs,
+			)
+
+			app.QueueUpdateDraw(func() {
+				textView.SetText(stats)
+			})
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	if err := app.SetRoot(textView, true).Run(); err != nil {
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,13 +1,15 @@
-module github.com/JoshuaGabriel/mytop
+module github.com/JoshuaGabriel/goup
 
 go 1.20
 
 require (
 	github.com/rivo/tview v0.0.0-20241103174730-c76f7879f592
 	github.com/shirou/gopsutil/v3 v3.24.5
+	github.com/urfave/cli/v2 v2.27.5
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.7.1 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
@@ -16,9 +18,11 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/tklauser/go-sysconf v0.3.14 // indirect
 	github.com/tklauser/numcpus v0.8.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/term v0.17.0 // indirect
