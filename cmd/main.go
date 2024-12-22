1d0cebda0451c5384ffdd55ec29f17672b1944ab
Joshua Blanch <42494981+JoshuaGabriel@users.noreply.github.com>
2024-12-22 09:02:08 +0800

    cmd: setup cli command (#2)
    * cmd: setup cli command
    
    Signed-off-by: Joshua Blanch <joshuamblanch@gmail.com>
    
    ---------
    
    Signed-off-by: Joshua Blanch <joshuamblanch@gmail.com>

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/urfave/cli/v2"
+
+	"syscall"
+)
+
+func Main(args []string) error {
+
+	cli.VersionFlag = &cli.BoolFlag{
+		Name: "version", Aliases: []string{"V"},
+		Usage: "print version only",
+	}
+	app := &cli.App{
+		Name:                 "goup",
+		Usage:                "Resource monitoring go application",
+		Copyright:            "Apache License 2.0",
+		HideHelpCommand:      true,
+		EnableBashCompletion: true,
+		Commands: []*cli.Command{
+			cmdMonitor(),
+		},
+	}
+
+	err := app.Run(args)
+	if errno, ok := err.(syscall.Errno); ok && errno == 0 {
+		err = nil
+	}
+	return err
+
+}
